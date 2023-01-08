2c6b483b0a2d1c6594202d210b050218b0d979d7
Taraska Rafał - Korpo <Rafal.Taraska@orange.com>
2023-01-08 22:50:52 +0100

    v1.1

--- data_manager/data_manager.py
+++ data_manager/data_manager.py
@@ -7,12 +7,14 @@ class DataManager:
     def __init__(self, csv_file_name):
         self.csv_file_name = csv_file_name
 
-    def get_from_csv(self):
-        with open(csv_file_1)as file: 
+    def get_from_csv(self, ):
+        with open(self.csv_file_name)as file: 
             csvFile = csv.reader(file, delimiter=',') 
             data_read = [row for row in csvFile]
             return data_read
 
-data_manager = DataManager(csv_file_name=csv_file_1)
+data_manager1 = DataManager(csv_file_name=csv_file_1)
+data_manager2 = DataManager(csv_file_name=csv_file_2)
 
-get = data_manager.get_from_csv()
+
+
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData, Date, Float
+from sqlalchemy import Table, Column, Integer, String, MetaData, Text, Float, insert
 from sqlalchemy import create_engine
-from data_manager.data_manager import data_manager
+from data_manager.data_manager import data_manager1, data_manager2
 
 engine = create_engine('sqlite:///database.db', echo=True)
 
@@ -8,25 +8,37 @@ meta = MetaData()
 
 clean_measure = Table(
    'clean_measure', meta,
-   Column('station', Integer, primary_key=True),
-   Column('date', Date),
-   Column('precip', Float),
-   Column('tobs', Integer)
+   Column('station', String, primary_key=True),
+   Column('date', String),
+   Column('precip', String),
+   Column('tobs', String)
+)
+
+clean_stations = Table(
+   'clean_stations', meta,
+   Column('station', String, primary_key=True),
+   Column('latitude', String),
+   Column('longitude', String),
+   Column('elevation', String),
+   Column('name', String),
+   Column('country', String),
+   Column('state', String),
 )
 
 meta.create_all(engine)
-print(engine.table_names())
+# print(engine.table_names())
 
-cleanMeasure = data_manager.get_from_csv()
+cleanMeasure = data_manager1.get_from_csv()
+cleanStations = data_manager2.get_from_csv()
+# x = cleanStations[0]
+# print(type(x[0]))
 
+for stations in cleanStations[1:]:
+      ins = clean_stations.insert().values(station=stations[0], latitude=stations[1], longitude=stations[2], elevation=stations[3], name=stations[4], country=stations[5], state=stations[6] )     
+      conn = engine.connect()
+      result = conn.execute(ins)
 
 for measure in cleanMeasure[1:]:
-    ins = clean_measure.insert()
-    ins = clean_measure.insert().values(station=measure[0], date=measure[1], precip=measure[2], tobs=measure[3] )
-    conn = engine.connect()
-    result = conn.execute(ins)
-    conn.execute(ins, [
-    {'station': measure[0], 'date': measure[1], 'precip':measure[2], 'tobs': measure[3] }
-    ])
-
-
+      ins = clean_measure.insert().values(station=measure[0], date=measure[1], precip=measure[2], tobs=measure[3] )     
+      conn = engine.connect()
+      result = conn.execute(ins)
